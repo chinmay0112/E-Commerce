1e85e02fad34306be79580b501540e2cee0d526e
Chinmay Kulshreshtha <chinmay.kulshreshtha@netprophetsglobal.com>
2025-04-30 14:35:38 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
