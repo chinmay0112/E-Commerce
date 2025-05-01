3ddd138679f189eb3e6433828aa8cea00f641a08
Chinmay Kulshreshtha <chinmay.kulshreshtha@netprophetsglobal.com>
2025-05-01 16:53:32 +0530

    best sellers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent, CarouselComponent],
+  imports: [HeaderComponent, FooterComponent, CarouselComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
