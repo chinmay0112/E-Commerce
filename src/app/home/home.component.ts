8fa4f7b0da535100be7cd316cf0539fe5315aee8
kartik saini <147996545+kartik6650@users.noreply.github.com>
2025-05-05 10:34:49 +0530

    category

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { ProductCategoryComponent } from "../product-category/product-category.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent, CarouselComponent],
+  imports: [HeaderComponent, FooterComponent, CarouselComponent, ProductCategoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
