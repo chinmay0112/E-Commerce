d1e99d89497340341c3428d39b88c9802a8200ad
Chinmay Kulshreshtha <chinmay.kulshreshtha@netprophetsglobal.com>
2025-05-07 11:28:23 +0530

    g

--- src/app/product-category/product-category.component.ts
+++ src/app/product-category/product-category.component.ts
@@ -6,60 +6,58 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-category',
-  imports: [CommonModule, TagModule, ButtonModule,Carousel],
+  imports: [CommonModule, TagModule, ButtonModule, Carousel],
   templateUrl: './product-category.component.html',
-  styleUrl: './product-category.component.scss'
+  styleUrl: './product-category.component.scss',
 })
 export class ProductCategoryComponent {
-  products: any[] =[];
+  products: any[] = [];
 
   responsiveOptions: any[] | undefined;
 
-  
-
   ngOnInit() {
     this.products = [
-        {
-          id: '1',
-          name: 'Pure Honey',
-  
-          image: 'assets/images/category-1.jpg',
-        },
-        {
-          id: '2',
-          name: 'Mustard Oil',
-  
-          image: 'assets/images/category-2.jpg',
-        },
-        {
-          id: '3',
-          name: 'Olive Oil',
-  
-          image: 'assets/images/category-3.jpg',
-        },
-      ];
+      {
+        id: '1',
+        name: 'Pure Honey',
 
-      this.responsiveOptions = [
-          {
-              breakpoint: '1400px',
-              numVisible: 2,
-              numScroll: 1,
-          },
-          {
-              breakpoint: '1199px',
-              numVisible: 3,
-              numScroll: 1,
-          },
-          {
-              breakpoint: '767px',
-              numVisible: 2,
-              numScroll: 1,
-          },
-          {
-              breakpoint: '575px',
-              numVisible: 1,
-              numScroll: 1,
-          },
-      ];
+        image: 'assets/images/category-1.jpg',
+      },
+      {
+        id: '2',
+        name: 'Mustard Oil',
+
+        image: 'assets/images/category-2.jpg',
+      },
+      {
+        id: '3',
+        name: 'Olive Oil',
+
+        image: 'assets/images/category-3.jpg',
+      },
+    ];
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '1199px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
   }
 }
